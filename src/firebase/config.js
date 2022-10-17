5977ff9d65a91fb4f064a7b0af4bb768d766c984
Julyme3 <jg0311@bk.ru>
2022-10-17 13:01:23 +0300

    firebase: add config

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAFC79uVfbCiZthe1ON6ZTCzrTuPayQwQ",
+  authDomain: "music-playlist-8b7ff.firebaseapp.com",
+  projectId: "music-playlist-8b7ff",
+  storageBucket: "music-playlist-8b7ff.appspot.com",
+  messagingSenderId: "68170853470",
+  appId: "1:68170853470:web:4de8a594437fe83df2d8cf",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
