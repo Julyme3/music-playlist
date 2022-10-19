38736a037de0fb06892df8ac6409ddf969251ce7
Julyme3 <jg0311@bk.ru>
2022-10-19 18:38:49 +0300

    add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <RouterView />
+  <Navbar />
+  <div class="content">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.content {
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 20px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="navbar">
+    <nav class="navbar-container">
+      <img class="navbar-img" src="@/assets/images/ninja.png" alt="Logo" />
+      <h1 class="navbar-title">
+        <router-link :to="{ name: 'Home' }">Music playlist</router-link>
+      </h1>
+      <div class="navbar-links">
+        <button class="btn" type="button">Logout</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background-color: white;
+
+  &-container {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  &-title {
+    margin-left: 20px;
+  }
+
+  &-links {
+    margin-left: auto;
+  }
+
+  .btn {
+    margin-left: 16px;
+    font-size: 14px;
+  }
+
+  &-img {
+    max-height: 60px;
+  }
+}
+</style>
