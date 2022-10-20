640f88d1346b9d3756c4cc086d7fd7ab25ed9861
Julyme3 <jg0311@bk.ru>
2022-10-20 20:11:56 +0300

    add List component

---
+++ src/components/List.vue
@@ -0,0 +1,63 @@
+<template>
+  <li v-for="playlist in playlists" :key="playlist.id" class="playlist">
+    <div class="playlist-thumbnail">
+      <img
+        class="playlist-img"
+        :src="playlist.coverUrl"
+        :alt="playlist.title"
+      />
+    </div>
+    <div class="playlist-info">
+      <h3 class="playlist-title">{{ playlist.title }}</h3>
+      <span>{{ playlist.userName }}</span>
+    </div>
+    <span class="playlist-song-number">{{ playlist.songs.length }}</span>
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  playlists: Object,
+});
+</script>
+
+<style scoped lang="less">
+.playlist {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: white;
+  margin: 16px 0;
+  transition: all ease 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 1px 2px 3px rgba(50, 50, 50, 0.05);
+    transform: scale(1.02);
+    transition: all ease 0.2s;
+  }
+
+  &-thumbnail {
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+    border-radius: 10px;
+  }
+
+  &-img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &-info {
+    margin: 0 30px;
+  }
+
+  &-song-number {
+    margin-left: auto;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <main>Hello</main>
+  <main class="home">
+    <span v-if="error" class="error">Could not fetch the playlists</span>
+    <ul v-if="documents" class="home-playlists">
+      <List :playlists="documents" />
+    </ul>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import getCollection from "@/composables/getCollection";
+import List from "@/components/List.vue";
+
+const { documents, error } = getCollection("playlists");
+</script>
