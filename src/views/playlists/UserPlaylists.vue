6e393c5865334b52826e5a9a3cd10fa176945b84
Julyme3 <jg0311@bk.ru>
2022-10-26 14:23:19 +0300

    displaying playlists by user

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="user-playlist">
-    <h2>My playlists.</h2>
+    <h2>My playlists</h2>
+    <List v-if="playlists" :playlists="playlists" />
+    <router-link :to="{ name: 'CreatePlaylist' }" class="btn"
+      >Create a new Playlist</router-link
+    >
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import getCollection from "@/composables/getCollection";
+import getUser from "@/composables/getUser";
+import List from "@/components/List.vue";
+
+const { user } = getUser();
+const { documents: playlists } = getCollection("playlists", [
+  "userId",
+  "==",
+  user.value.uid,
+]);
+</script>
 
 <style scoped lang="less"></style>
